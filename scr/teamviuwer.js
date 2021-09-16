73d846ebf5a8cc76e81f103aa35c2a872d66801f
Ellechugista <mapalaciosarjona@gmail.com>
2021-09-16 10:08:07 -0500

    1.0.0

---
+++ scr/teamviuwer.js
@@ -0,0 +1,29 @@
+window.sr = ScrollReveal();
+
+    sr.reveal("#cabeza", {
+        duration:2000,
+        origin: "bottom",
+        distance: "-50px"
+
+    });
+
+    sr.reveal("#navegacion", {
+        duration:3000,
+        origin: "left",
+        distance: "200px"
+
+    });
+
+    sr.reveal("#prime", {
+        duration:3000,
+        origin: "left",
+        distance: "200px"
+
+    });
+
+    sr.reveal("#secont, .articulo", {
+        duration:2500,
+        origin: "top",
+        distance: "-50px"
+
+    });
